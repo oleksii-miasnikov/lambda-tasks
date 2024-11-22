6a526156716cb0e9af1de0576a8584f333aac99e
oleksii-miasnikov <oleksii_miasnikov@epam.com>
2024-11-22 13:25:39 +0200

    #03 task02-03

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -22,28 +22,13 @@ import java.util.Map;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
-
-		String path = (String) event.get("rawPath");
-		System.out.println("Endpoint: " + path);
-
-		Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
-		Map<String, String> http = (Map<String, String>) requestContext.get("http");
-		String method = http.get("method");
-		System.out.println("Method: " + method);
-
 		Map<String, Object> resultMap = new HashMap<String, Object>();
-		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
-			resultMap.put("statusCode", 200);
-			resultMap.put("message", "Hello from Lambda");
-		} else {
-			resultMap.put("statusCode", 400);
-			resultMap.put("message", "Bad request syntax or unsupported method. Request path: "
-					+ path + ". HTTP method: " + method);
-		}
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
 		return resultMap;
 	}
 }
