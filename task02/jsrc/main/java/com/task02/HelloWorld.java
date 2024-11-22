1d4d72e65e947640d6be65e1d39e35d6a8c84c49
oleksii-miasnikov <oleksii_miasnikov@epam.com>
2024-11-22 14:25:24 +0200

    #03 task02-05

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -46,13 +46,13 @@ public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGate
 		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
 			return APIGatewayV2HTTPResponse.builder()
 					.withStatusCode(200)
-					.withBody("{\"statusCode\":200, \"message\": \"Hello from Lambda\"}")
+					.withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}")
 					.build();
 		}
 
 		return APIGatewayV2HTTPResponse.builder()
 				.withStatusCode(400)
-				.withBody(String.format("{\"statusCode\":400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method))
+				.withBody(String.format("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method))
 				.build();
 	}
 }
