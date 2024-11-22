c45a934f2181ba8908cbcd4fe244ea704a848e96
oleksii-miasnikov <oleksii_miasnikov@epam.com>
2024-11-22 14:11:45 +0200

    #03 task02-04

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -1,15 +1,25 @@
 package com.task02;
 
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.HashMap;
+
 
 @LambdaHandler(
     lambdaName = "hello_world",
@@ -22,13 +32,27 @@ import java.util.Map;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+	@Override
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+		System.out.println("---Hello from lambda---");
+		String method = requestEvent.getRequestContext().getHttp().getMethod();
+		String path = requestEvent.getRawPath();
+		System.out.println("Method --> " + method);
+		System.out.println("Path --> " + path);
+
+		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+			return APIGatewayV2HTTPResponse.builder()
+					.withStatusCode(200)
+					.withBody("{\"message\": \"Hello from Lambda\"}")
+					.build();
+		}
+
+		return APIGatewayV2HTTPResponse.builder()
+				.withStatusCode(400)
+				.withBody(String.format("{\"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method))
+				.build();
 	}
 }
