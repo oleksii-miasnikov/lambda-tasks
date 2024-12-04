1ba6bade321ee33fd73104ccf446d0a37af7b051
oleksii-miasnikov <oleksii_miasnikov@epam.com>
2024-12-04 20:55:48 +0200

    #18 task06 dynemodb trigger started

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -4,6 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,13 +20,30 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class AuditProducer implements RequestHandler<Object, Map<String, Object>> {
+@DynamoDbTriggerEventSource(
+		targetTable = "Configuration",
+		batchSize = 123
+)
+@DependsOn(
+		name = "Configuration",
+		resourceType = ResourceType.DYNAMODB_TABLE
+)
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
+
+	private final ObjectMapper objectMapper;
+
+	public AuditProducer() {
+		this.objectMapper = new ObjectMapper();
+	}
+
+	public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
+		context.getLogger().log("handleRequest started");
+		context.getLogger().log("dynamodbEvent" + dynamodbEvent);
+		context.getLogger().log("context" + context);
+		for (DynamodbEvent.DynamodbStreamRecord record : dynamodbEvent.getRecords()) {
+			context.getLogger().log("Incoming record: " + record);
+		}
+		return null;
 	}
 }
