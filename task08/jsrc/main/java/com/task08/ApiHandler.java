0095ec9e598d13c7cec54ce22d3a5bf34ca9a50b
oleksii-miasnikov <oleksii_miasnikov@epam.com>
2024-12-08 14:05:53 +0200

    #23 task08 completed

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -3,7 +3,12 @@ package com.task08;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.task08.utils.WeatherForecast;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,17 +16,27 @@ import java.util.Map;
 @LambdaHandler(
     lambdaName = "api_handler",
 	roleName = "api_handler-role",
+	layers = {"sdk-layer"},
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
+@LambdaLayer(
+		layerName = "sdk-layer",
+		libraries = {"lib/weather-forecast-1.0.jar"},
+		runtime = DeploymentRuntime.JAVA11,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
+)
+public class ApiHandler implements RequestHandler<Object, String> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+	private static final String URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+	public String handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+		WeatherForecast weatherForecast = new WeatherForecast();
+		String forecast = weatherForecast.getWeatherForecast(URL);
+		System.out.println("Weather forecast:" + forecast);
+		return forecast;
 	}
 }
